23e9f7dab6f59acd5ffb216c9988b464ebd98ddd
Fabiano Rosas <fabianorosas@gmail.com>
2019-01-22 10:26:35 -0500

    Fix job control issues
    From https://busybox.net/FAQ.html#job_control:
    
    "Job control will be turned off since your shell can not obtain a
    controlling terminal. This typically happens when you run your shell
    on /dev/console. The kernel will not provide a controlling terminal
    on the /dev/console device. You should run your shell on a normal tty
    such as tty1 or ttyS0 and everything will work."

--- init.sh
+++ init.sh
@@ -5,4 +5,5 @@ mount -t proc proc proc
 mount -t devtmpfs devtmpfs dev
 mount -t sysfs sysfs sys
 mount -t debugfs none sys/kernel/debug
-/bin/sh
+# https://busybox.net/FAQ.html#job_control
+setsid /bin/sh -c 'exec sh </dev/hvc0 >/dev/hvc0 2>&1'
